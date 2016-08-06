fc1d70fc2a00d70a35995b0a31196159e77d0cc4
Jeroen Jansen <Jeroen Jansen>
2016-08-06 18:01:29 +0200

    start of sending mail test

--- js/controllers/contactController.js
+++ js/controllers/contactController.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
 
-    angular.module('controllers').controller('contactController', ['menuFactory',
-        function(menuFactory) {
+    angular.module('controllers').controller('contactController', ['$http', 'menuFactory',
+        function($http, menuFactory) {
             var contactModel = this;
 
             menuFactory.setActivePage('contact');
@@ -10,11 +10,36 @@
             initialize();
 
             /**
+             * Model functions
+             */
+
+            contactModel.sendMail = function() {
+                $http.post('php/sendMail.php', JSON.stringify(contactModel.mailModel))
+                    .success(
+                        function(data) {
+                            alert('Mail sent!');
+                        })
+                    .error(
+                        function(data) {
+                            alert('Unable to sent mail!');
+                        });
+            };
+
+            /**
              * Helper functions
              */
 
             function initialize() {
                 hideModalBackdrop();
+
+                createMailModel();
+            }
+
+            function createMailModel() {
+                contactModel.mailModel = {};
+                contactModel.mailModel.email = '';
+                contactModel.mailModel.subject = '';
+                contactModel.mailModel.message = '';
             }
 
             function hideModalBackdrop() {
--- php/sendMail.php
+++ php/sendMail.php
@@ -1,13 +1,16 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Content-Type: application/json');
 
+$array = json_decode(file_get_contents('php://input'), true);
+$email = $array['email'];
+$subject = $array['subject'];
+$message = $array['message'];
 $to = "Jansen.Jeroen@hotmail.com";
 
-mail ($to, $subject, $message, "From: " . $email);
+mail ($to, $subject, $message, "From: $email");
 echo "Your message has been sent";
 
 ?>
