7ecdfd06eacae1fdc1c0c2d60eb4327897ddf7e3
Jeroen Jansen <Jansen.Jeroen@hotmail.com>
2017-10-24 23:44:47 +0200

    Minification van bootstrap.css verbetered

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,13 +44,7 @@ module.exports = function(grunt) {
           {expand: true, src: ['css/bootstrap_overlay.css'], dest: 'dest/'},
           {expand: true, src: ['vendors/bootstrap/css/bootstrap.min.css'], dest: 'dest/'},
           {expand: true, src: ['js/jeugdhuis_bastion.min.js'], dest: 'dest/'},
-          {expand: true, src: ['vendors/animate/animate.css'], dest: 'dest/'},
-          {expand: true, src: ['vendors/jquery/jquery-2.1.1.min.js'], dest: 'dest/'},
-          {expand: true, src: ['vendors/angularJS/angular.js'], dest: 'dest/'},
-          {expand: true, src: ['vendors/angularJS/angular-route.js'], dest: 'dest/'},
-          {expand: true, src: ['vendors/angularJS/angular-animate.js'], dest: 'dest/'},
-          {expand: true, src: ['vendors/bootstrap/js/bootstrap.min.js'], dest: 'dest/'},
-          {expand: true, src: ['vendors/bootstrap/js/ui-bootstrap-tpls-2.0.2.min.js'], dest: 'dest/'}
+          {expand: true, src: ['vendors/**/*'], dest: 'dest/'}
         ],
       },
     }
